14e5f6ac82c808d7d4c594c85ac6f22a464d5cfc
LuizPedroSousa <luizpedrosousa64@gmail.com>
2021-09-25 15:11:25 -0300

    ci(unit): added github actions config :green_heart:

---
+++ __tests__/index.test.tsx
@@ -0,0 +1,30 @@
+/* eslint-disable no-import-assign */
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import Home from '../src/pages/index'
+
+jest.mock('next/router', () => ({
+  withRouter: component => {
+    component.defaultProps = {
+      ...component.defaultProps,
+      router: {
+        route: '/',
+        pathname: '',
+        query: '',
+        asPath: ''
+      }
+    }
+
+    return component
+  }
+}))
+
+describe('Home', () => {
+  it('renders a main with cards container', () => {
+    render(<Home />)
+
+    const main = screen.getByRole('main')
+
+    expect(main).toBeInTheDocument()
+  })
+})
